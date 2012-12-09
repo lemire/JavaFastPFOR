cb0959bc01767c5cfe1f786cf5db3a09c0bd85ee
Di Wu <dwu@palantir.com>
2012-12-08 17:11:01 -0800

    toString and number

--- src/integercompression/IntWrapper.java
+++ src/integercompression/IntWrapper.java
@@ -3,7 +3,8 @@ package integercompression;
 /**
  * @author dwu
  */
-public final class IntWrapper {
+public final class IntWrapper extends Number {
+    private static final long serialVersionUID = 1L;
     private int value;
 
     public IntWrapper() {
@@ -14,7 +15,35 @@ public final class IntWrapper {
         this.value = value;
     }
 
+    public void add(int value) {
+        this.value += value;
+    }
+
+    @Override
+    public double doubleValue() {
+        return this.value;
+    }
+
+    @Override
+    public float floatValue() {
+        return this.value;
+    }
+
     public int get() {
+        return this.value;
+    }
+
+    public void increment() {
+        this.value++;
+    }
+
+    @Override
+    public int intValue() {
+        return this.value;
+    }
+
+    @Override
+    public long longValue() {
         return this.value;
     }
 
@@ -22,11 +51,8 @@ public final class IntWrapper {
         this.value = value;
     }
 
-    public void add(int value) {
-        this.value += value;
-    }
-
-    public void increment() {
-        this.value++;
+    @Override
+    public String toString() {
+        return Integer.toString(this.value);
     }
 }
