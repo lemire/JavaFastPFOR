55455a7e5889759f90abb844d0872bbdbfc9bf9a
Daniel Lemire <lemire@gmail.com>
2013-04-23 08:33:03 -0400

    Fixing typo in license

--- src/integercompression/IntegratedBitPacking.java
+++ src/integercompression/IntegratedBitPacking.java
@@ -1,4 +1,9 @@
-
+/**
+ * This code is released under the
+ * Apache License Version 2.0 http://www.apache.org/licenses/.
+ *
+ * (c) Daniel Lemire, http://lemire.me/en/
+ */
 
 package integercompression;
 
--- src/integercompression/IntegratedVariableByte.java
+++ src/integercompression/IntegratedVariableByte.java
@@ -1,3 +1,10 @@
+
+/**
+ * This code is released under the
+ * Apache License Version 2.0 http://www.apache.org/licenses/.
+ *
+ * (c) Daniel Lemire, http://lemire.me/en/
+ */
 package integercompression;
 
 import java.nio.ByteBuffer;
