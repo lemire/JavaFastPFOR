c523646dfb2cbd4cd783da410772c7bbc6357286
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-09-05 03:24:28 +0900

    make array extension clearly

--- src/test/java/me/lemire/integercompression/TestUtils.java
+++ src/test/java/me/lemire/integercompression/TestUtils.java
@@ -22,14 +22,15 @@ public class TestUtils {
         // Example:
         //  - VariableByte compresses an array like [ -1 ].
         //  - Composition compresses a short array.
+        final int EXTEND = 1;
 
-        int[] compressed = new int[orig.length + 1];
+        int[] compressed = new int[orig.length + EXTEND];
         IntWrapper c_inpos = new IntWrapper(0);
         IntWrapper c_outpos = new IntWrapper(0);
         codec.compress(orig, c_inpos, orig.length, compressed,
                 c_outpos);
 
-        assertTrue(c_outpos.get() <= orig.length + 1);
+        assertTrue(c_outpos.get() <= orig.length + EXTEND);
 
         // Uncompress an array.
         int[] uncompressed = new int[orig.length];
