e08da5120c2f037b5062cb5481a493800fa5fbf9
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-09-05 23:31:26 +0900

    refactor BasicTest#verifyWithoutMask

--- src/test/java/me/lemire/integercompression/BasicTest.java
+++ src/test/java/me/lemire/integercompression/BasicTest.java
@@ -49,21 +49,19 @@ public class BasicTest
     @Test
     public void verifyWithoutMask() {
         final int N = 32;
-        final int times = 1000;
+        final int TIMES = 1000;
         Random r = new Random();
         int[] data = new int[N];
         int[] compressed = new int[N];
         int[] uncompressed = new int[N];
         for (int bit = 0; bit < 31; ++bit) {
-            for (int t = 0; t < times; ++t) {
+            for (int t = 0; t < TIMES; ++t) {
                 for (int k = 0; k < N; ++k) {
                     data[k] = r.nextInt(1 << bit);
                 }
                 BitPacking.fastpackwithoutmask(data, 0, compressed, 0, bit);
                 BitPacking.fastunpack(compressed, 0, uncompressed, 0, bit);
-                if (!Arrays.equals(uncompressed, data)) {
-                    throw new RuntimeException("bug " + bit);
-                }
+                assertArrayEquals(uncompressed, data);
             }
         }
     }
