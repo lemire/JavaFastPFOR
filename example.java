0aadfd52d41a9e624b8afb7024052a6724a8ebe7
Daniel Lemire <lemire@gmail.com>
2015-08-31 06:45:47 -0400

    Adding headless demo.

--- example.java
+++ example.java
@@ -7,8 +7,9 @@ public class example {
         unsortedExample();
         basicExample();
         advancedExample();
+        headlessDemo();
     }
-    
+
     public static void basicExample() {
         int[] data = new int[2342351];
         System.out.println("Compressing "+data.length+" integers in one go");
@@ -19,16 +20,16 @@ public class example {
         // Very important: the data is in sorted order!!! If not, you
         // will get very poor compression with IntegratedBinaryPacking,
         // you should use another CODEC.
-          
+
         // next we compose a CODEC. Most of the processing
         // will be done with binary packing, and leftovers will
         // be processed using variable byte
-        IntegratedIntegerCODEC codec =  new 
+        IntegratedIntegerCODEC codec =  new
            IntegratedComposition(
                     new IntegratedBinaryPacking(),
                     new IntegratedVariableByte());
         // output vector should be large enough...
-        int [] compressed = new int[data.length+1024]; 
+        int [] compressed = new int[data.length+1024];
         // compressed might not be large enough in some cases
         // if you get java.lang.ArrayIndexOutOfBoundsException, try
         // allocating more memory
@@ -41,7 +42,7 @@ public class example {
         IntWrapper inputoffset = new IntWrapper(0);
         IntWrapper outputoffset = new IntWrapper(0);
         codec.compress(data,inputoffset,data.length,compressed,outputoffset);
-        // got it! 
+        // got it!
         // inputoffset should be at data.length but outputoffset tells
         // us where we are...
         System.out.println("compressed from "+data.length*4/1024+"KB to "+outputoffset.intValue()*4/1024+"KB");
@@ -56,13 +57,13 @@ public class example {
         int[] recovered = new int[data.length];
         IntWrapper recoffset = new IntWrapper(0);
         codec.uncompress(compressed,new IntWrapper(0),compressed.length,recovered,recoffset);
-        if(Arrays.equals(data,recovered)) 
+        if(Arrays.equals(data,recovered))
           System.out.println("data is recovered without loss");
         else
           throw new RuntimeException("bug"); // could use assert
         System.out.println();
     }
-    
+
     /**
     * This is an example to show you can compress unsorted integers
     * as long as most are small.
@@ -76,7 +77,7 @@ public class example {
         for(int k = 0; k < N; k+=5) data[k] = 100;
         for(int k = 0; k < N; k+=533) data[k] = 10000;
         int[] compressed = new int [N+1024];// could need more
-        IntegerCODEC codec =  new 
+        IntegerCODEC codec =  new
            Composition(
                     new FastPFOR(),
                     new VariableByte());
@@ -91,7 +92,7 @@ public class example {
         int[] recovered = new int[N];
         IntWrapper recoffset = new IntWrapper(0);
         codec.uncompress(compressed,new IntWrapper(0),compressed.length,recovered,recoffset);
-        if(Arrays.equals(data,recovered)) 
+        if(Arrays.equals(data,recovered))
           System.out.println("data is recovered without loss");
         else
           throw new RuntimeException("bug"); // could use assert
@@ -100,10 +101,10 @@ public class example {
     }
 
     /**
-    * This is like the basic example, but we 
+    * This is like the basic example, but we
     * show how to process larger arrays in chunks.
     *
-    * Some of this code was written by Pavel Klinov. 
+    * Some of this code was written by Pavel Klinov.
     */
     public static void advancedExample() {
         int TotalSize = 2342351; // some arbitrary number
@@ -119,15 +120,15 @@ public class example {
         // will be done with binary packing, and leftovers will
         // be processed using variable byte, using variable byte
         // only for the last chunk!
-        IntegratedIntegerCODEC regularcodec =  new 
+        IntegratedIntegerCODEC regularcodec =  new
                    IntegratedBinaryPacking();
         IntegratedVariableByte ivb = new IntegratedVariableByte();
-        IntegratedIntegerCODEC lastcodec =  new 
+        IntegratedIntegerCODEC lastcodec =  new
            IntegratedComposition(regularcodec,ivb);
         // output vector should be large enough...
         int [] compressed = new int[TotalSize+1024];
-        
-        
+
+
         /**
         *
         * compressing
@@ -135,24 +136,24 @@ public class example {
         */
         IntWrapper inputoffset = new IntWrapper(0);
         IntWrapper outputoffset = new IntWrapper(0);
-        for(int k = 0; k < TotalSize / ChunkSize; ++k) 
+        for(int k = 0; k < TotalSize / ChunkSize; ++k)
             regularcodec.compress(data,inputoffset,ChunkSize,compressed,outputoffset);
          lastcodec.compress(data, inputoffset, TotalSize % ChunkSize, compressed, outputoffset);
-        // got it! 
+        // got it!
         // inputoffset should be at data.length but outputoffset tells
         // us where we are...
         System.out.println("compressed from "+data.length*4/1024+"KB to "+outputoffset.intValue()*4/1024+"KB");
         // we can repack the data:
         compressed = Arrays.copyOf(compressed,outputoffset.intValue());
-        
-        
+
+
         /**
         *
         * now uncompressing
         *
         * We are *not* assuming that the original array length is known, however
         * we assume that the chunk size (ChunkSize) is known.
-        * 
+        *
         */
         int[] recovered = new int[ChunkSize];
         IntWrapper compoff = new IntWrapper(0);
@@ -162,7 +163,7 @@ public class example {
         while(compoff.get()<compressed.length) {
             recoffset = new IntWrapper(0);
             regularcodec.uncompress(compressed,compoff,compressed.length - compoff.get(),recovered,recoffset);
-            
+
             if(recoffset.get() < ChunkSize) {// last chunk detected
                 ivb.uncompress(compressed,compoff,compressed.length - compoff.get(),recovered,recoffset);
             }
@@ -175,4 +176,48 @@ public class example {
         System.out.println();
 
     }
+
+
+ /*
+ *  Demo of the headless approach where we must supply the array length
+ */
+ public static void headlessDemo() {
+    System.out.println("Compressing arrays with minimal header...");
+    int[] uncompressed1 = {1,2,1,3,1};
+    int[] uncompressed2 = {3,2,4,6,1};
+
+    int[] compressed = new int[uncompressed1.length+uncompressed2.length+1024];
+
+    SkippableIntegerCODEC codec = new SkippableComposition(new BinaryPacking(), new VariableByte());
+
+    // compressing
+    IntWrapper outPos = new IntWrapper();
+
+    IntWrapper previous = new IntWrapper();
+
+    codec.headlessCompress(uncompressed1,new IntWrapper(),uncompressed1.length,compressed,outPos);
+    int length1 = outPos.get() - previous.get();
+    previous = new IntWrapper(outPos.get());
+    codec.headlessCompress(uncompressed2,new IntWrapper(),uncompressed2.length,compressed,outPos);
+    int length2 = outPos.get() - previous.get();
+
+
+
+    compressed = Arrays.copyOf(compressed,length1 + length2);
+    System.out.println("compressed unsorted integers from "+uncompressed1.length*4+"B to "+length1*4+"B");
+    System.out.println("compressed unsorted integers from "+uncompressed2.length*4+"B to "+length2*4+"B");
+    System.out.println("Total compressed output "+compressed.length);
+
+    int[] recovered1 = new int[uncompressed1.length];
+    int[] recovered2 = new int[uncompressed1.length];
+    IntWrapper inPos = new IntWrapper();
+    System.out.println("Decoding first array starting at pos = "+inPos);
+    codec.headlessUncompress(compressed,inPos, compressed.length, recovered1, new IntWrapper(0), uncompressed1.length);
+    System.out.println("Decoding second array starting at pos = "+inPos);
+    codec.headlessUncompress(compressed,inPos, compressed.length, recovered2, new IntWrapper(0), uncompressed2.length);
+    if(!Arrays.equals(uncompressed1,recovered1)) throw new RuntimeException("First array does not match.");
+    if(!Arrays.equals(uncompressed2,recovered2)) throw new RuntimeException("Second array does not match.");
+    System.out.println("The arrays match, your code is probably ok.");
+
+ }
 }
